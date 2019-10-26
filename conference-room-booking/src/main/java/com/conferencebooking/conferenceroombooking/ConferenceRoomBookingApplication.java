72a2a40f2c05164523531932a33cc0cb7b5a1f6c
DobryiD <dimadob9@gmail.com>
2019-10-26 12:23:16 +0200

    Basic setup of the project

---
+++ conference-room-booking/src/main/java/com/conferencebooking/conferenceroombooking/ConferenceRoomBookingApplication.java
@@ -0,0 +1,13 @@
+package com.conferencebooking.conferenceroombooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRoomBookingApplication.class, args);
+	}
+
+}
